31b0df8efc6099b200ee570f5a168583d59ebc49
Tamiris Shigaki <shigakiendo@gmail.com>
2022-07-05 17:54:55 -0300

    :test_tube: test: função insertProduct, camada Controllers

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -4,38 +4,34 @@ const productService = require("../../../services/productService");
 const productController = require("../../../controllers/productController");
 const { products, product } = require("../../../helpers/productsMock");
 
-describe("Arquivo Controller", () => {
+describe('Arquivo Controller', () => {
   const res = {};
   const req = {};
 
   before(() => {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub();
-
-    sinon.stub(productService, "listAll").resolves(products);
   });
 
   after(() => {
     sinon.restore();
   });
 
-  describe("#listAll", () => {
-    it("verifica se retorna o status 200", async () => {
+  describe('#listAll', () => {
+    before(() => {
+      sinon.stub(productService, "listAll").resolves(products);
+    });
+
+    it('verifica se retorna o status 200', async () => {
       await productController.listAll(req, res);
 
       expect(res.status.calledWith(200)).to.be.equal(true);
     });
 
-    it("verifica se retorna um json com um array de objetos", async () => {
+    it('verifica se retorna um json com um array de objetos', async () => {
       await productController.listAll(req, res);
 
-      products.forEach((product) => expect(product).to.be.an("object"));
-    });
-
-    it("verifica se retorna um json com um array de objetos", async () => {
-      await productController.listAll(req, res);
-
-      products.forEach((product) => expect(product).to.be.an("object"));
+      products.forEach((product) => expect(product).to.be.an('object'));
     });
 
     it("verifica se os objetos retornados pelo array possuem id e nome", async () => {
@@ -45,9 +41,9 @@ describe("Arquivo Controller", () => {
     });
   });
 
-  describe("#getById", () => {
-    before(() => {
-      req.params = { id: 1 };
+  describe('#getById', () => {
+    beforeEach(() => {
+      req.params = {id: 1}
     });
 
     it("verifica se retorna o status 200", async () => {
@@ -58,6 +54,24 @@ describe("Arquivo Controller", () => {
 
     it("verifica se retorna um json com um array de objetos e se possui id e name", async () => {
       await productController.getById(req, res);
+
+      expect(res.json).to.have.keys[("id", "name")];
+    });
+  });
+
+  describe("#insertProduct", () => {
+    beforeEach(() => {
+      req.body = { name: 'ProdutoY' };
+    });
+
+    it("verifica se chama o método status 201", async () => {
+      await productController.insertProduct(req, res);
+
+      expect(res.status.calledWith(201)).to.be.equal(true);
+    });
+
+    it("verifica se retorna um json com um array de objetos e se possui id e name", async () => {
+      await productController.insertProduct(req, res);
 
       expect(res.json).to.have.keys[("id", "name")];
     });
